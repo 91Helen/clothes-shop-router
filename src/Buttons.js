ffe3a1bf9bf7308d82084ee27e586ab6a082abb7
91Helen <157799017+91Helen@users.noreply.github.com>
2025-03-28 11:48:31 +0500

    Update Buttons.js

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,7 +1,13 @@
+import { data } from "react-router-dom";
+import { useState } from "react";
+
+
 function Buttons ({filteredClothes}) {
+   const [products, setProducts] = useState(data);
     
     return(
         <div className="cont">
+          <button onClick={() => setProducts(data)}>Show all</button> 
        <button className="change" onClick={() => filteredClothes("dress")}>Dresses</button>
        <button className="change" onClick={() => filteredClothes("pants")}>Pants</button>
        <button className="change" onClick={() => filteredClothes("skirt")}>Skirts</button>
